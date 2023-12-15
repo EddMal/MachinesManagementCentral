1059df80e39db1f7b84c4fa5e75aa3133123d154
EddMal <107246914+EddMal@users.noreply.github.com>
2023-12-15 14:26:02 +0100

    Add project files.

---
+++ MachinesManagementCentral/Client/Components/DeviceList.razor.cs
@@ -0,0 +1,21 @@
+﻿using MachinesManagementCentral.Shared;
+using MachinesManagementCentral.Shared.Domains;
+namespace MachinesManagementCentral.Client.Components
+{
+    public partial class DeviceList
+    {
+        public List<Device> DevisteLst { get; set; } = new List<Device>();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            DevisteLst.Add(new Device() { DeviceId = Guid.NewGuid(),Location = Location.Sweden, Date =DateTime.Now, DeviceType ="CoffeeMakerx4000", Status = Status.offline });
+            DevisteLst.Add(new Device() { DeviceId = Guid.NewGuid(), Location = Location.England, Date = DateTime.Now, DeviceType = "Saw", Status = Status.offline });
+
+        }
+
+
+
+    }
+}
---
+++ MachinesManagementCentral/Shared/Domains/Device.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachinesManagementCentral.Shared.Domains
+{
+    public enum Status { 
+    offline,
+    online
+    }
+
+    public enum Location
+    {
+        Sweden,
+        England
+    }
+
+    public class Device
+    {
+
+        public Guid DeviceId { get; set; }
+        public Location Location { get; set; }
+        public DateTime Date { get; set; }
+        public string DeviceType { get; set; } = string.Empty;
+
+        public Status Status { get; set; }
+
+
+
+    }
+}
